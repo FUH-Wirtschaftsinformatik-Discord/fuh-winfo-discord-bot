0820f8d68b7603b2cb30f038e571a770c3979b97
Your Name <you@example.com>
2025-08-24 17:05:58 +0200

    fix: send welcome message

--- extensions/welcome.py
+++ extensions/welcome.py
@@ -23,16 +23,14 @@ class Welcome(commands.Cog):
 schön, dass du hergefunden hast :nerd: 
 
 Unsere Serverregeln findest du hier: <#{os.getenv('DISCORD_RULE_CHANNEL')}> 
-Erfahre mehr über die Funktionalitäten des Servers und des Bots hier: <#{os.getenv('DISCORD_BOT_MANUAL_CHANNEL')}>
 Antworten auf häufig gestellte Fragen findest du hier: <#{os.getenv('DISCORD_FAQ_CHANNEL')}>
+Weitere Informationen zu den Funktionen des Servers und des Bots findest du hier: <#{os.getenv('DISCORD_BOT_MANUAL_CHANNEL')}>
 Zur Lerngruppen-Suche geht es hier lang: <#{os.getenv('DISCORD_LEARNINGGROUPS_POST')}> 
 Quatschen kannst du hier: ⁠<#{os.getenv('DISCORD_CHATTING_CHANNEL_1')}> oder ⁠<#{os.getenv('DISCORD_CHATTING_CHANNEL_2')}> 
 Stelle gerne Fragen an die Admins/Moderation/den Bot per DM oder einfach hier: <#{os.getenv('DISCORD_QUESTIONS_AND_ANSWERS_CHANNEL')}>       
         """
 
         await channel.send(welcome_message)
-        # await channel.send(random.choice(welcome_messages))
-
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Welcome(bot))
