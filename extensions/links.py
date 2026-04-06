38b1695a1169c11e07aa7892de0544107d3ba6ee
Your Name <you@example.com>
2026-04-06 21:08:56 +0200

    fix: show links

--- extensions/links.py
+++ extensions/links.py
@@ -13,7 +13,7 @@ class Links(commands.GroupCog, name="links", description="Linkverwaltung für Ka
 
     @app_commands.command(name="show", description="Zeige Links für diesen Kanal an.")
     @app_commands.describe(category="Zeige nur Links für diese Kategorie an.", public="Zeige die Linkliste für alle.")
-    async def cmd_show(self, interaction: Interaction, category: str = None, public: bool = True):
+    async def cmd_show(self, interaction: Interaction, category: str = None, public: bool = False):
         await interaction.response.defer(ephemeral=not public)
 
         message = "### __Folgende Links sind in diesem Channel hinterlegt__\n"
--- push-images.ps1
+++ push-images.ps1
@@ -1,8 +1,8 @@
-docker build -f .\bot.Dockerfile -t rndintusr/fuh-winfo-discordbot:v1.3.0 .
-docker push rndintusr/fuh-winfo-discordbot:v1.3.0
+docker build -f .\bot.Dockerfile -t rndintusr/fuh-winfo-discordbot:v1.3.2 .
+docker push rndintusr/fuh-winfo-discordbot:v1.3.2
 
-docker build -f .\scraper.Dockerfile -t rndintusr/fuh-winfo-discordbot-grade-scraper:v1.3.0 .
-docker push rndintusr/fuh-winfo-discordbot-grade-scraper:v1.3.0
+docker build -f .\scraper.Dockerfile -t rndintusr/fuh-winfo-discordbot-grade-scraper:v1.3.2 .
+docker push rndintusr/fuh-winfo-discordbot-grade-scraper:v1.3.2
 
-docker build -f .\plotter.Dockerfile -t rndintusr/fuh-winfo-discordbot-grade-plotter:v1.3.0 .
-docker push rndintusr/fuh-winfo-discordbot-grade-plotter:v1.3.0
+docker build -f .\plotter.Dockerfile -t rndintusr/fuh-winfo-discordbot-grade-plotter:v1.3.2 .
+docker push rndintusr/fuh-winfo-discordbot-grade-plotter:v1.3.2
